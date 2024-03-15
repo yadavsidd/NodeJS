85899c7976b102b5141a92139c4dfb9400cc06f6
Sidd <sidddhant.10@gmail.com>
2024-03-16 00:07:33 +0530

    Functions and JS Syntax

---
+++ java-script/function.js
@@ -0,0 +1,5 @@
+function summarizeUser(userName,userAge,userHasHobby){
+return(
+    'name is'+ userName+ 'User age is'+userAge+'user has hobbys'+userHasHobby);
+}
+console.log(summarizeUser(userName,userHasHobby));
---
+++ java-script/syntax.js
@@ -0,0 +1,15 @@
+
+//VARIABLE DECLERATION//
+
+var userName = "max";
+var userAge = "19";
+var userHasHobby = true;
+
+
+//function//
+
+function summarizeUser(userName,userAge,userHasHobby){
+    return(
+        'name is '+ userName+ ' User age is '+userAge+' user has hobbys '+userHasHobby);
+    }
+    console.log(summarizeUser(userName,userHasHobby));
